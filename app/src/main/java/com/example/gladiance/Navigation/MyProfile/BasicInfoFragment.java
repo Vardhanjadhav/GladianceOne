9ed657fb535c5bad9ca414ae92996b2a0afa1b87
vardhan <vardhan.j@velocetechinsights.com>
2023-12-28 17:06:24 +0530

    28-12-2023 UI Login and MyProfile Changes

--- app/src/main/java/com/example/gladiance/Navigation/MyProfile/BasicInfoFragment.java
+++ app/src/main/java/com/example/gladiance/Navigation/MyProfile/BasicInfoFragment.java
@@ -1,6 +1,9 @@
 package com.example.gladiance.Navigation.MyProfile;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,10 +12,17 @@ import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.gladiance.Login.LoginActivity;
 import com.example.gladiance.R;
+
+import java.util.Objects;
 
 public class BasicInfoFragment extends Fragment {
 
@@ -25,6 +35,11 @@ public class BasicInfoFragment extends Fragment {
     LinearLayout llInviteUser;
     LinearLayout llJoinUser;
     LinearLayout llLogout;
+    LinearLayout editProfile;
+
+    TextView tvMessage;
+
+    Button btnYes,btnNo;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -72,15 +87,61 @@ public class BasicInfoFragment extends Fragment {
         });
 
         llLogout = view.findViewById(R.id.llLogout);
-        llLogout.setOnClickListener(new View.OnClickListener() {
+
+
+        editProfile = view.findViewById(R.id.editProfile);
+        editProfile.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), LoginActivity.class);
-                startActivity(intent);
+            public void onClick(View v) {
+                Fragment fragment = new EditProfileFragment();
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager()
+                        .beginTransaction();
+
+                transaction.replace(R.id.basic_info_frameLayout, fragment)
+                        .commit();
             }
         });
 
+        llLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message = "Are you sure you want to log out from gladiance";
+                showCustomDialogBox(message);
+            }
 
+            private void showCustomDialogBox(String message) {
+
+                final Dialog dialog = new Dialog(requireContext());
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setCancelable(false);
+                dialog.setContentView(R.layout.layout_custom_dailog);
+                Objects.requireNonNull(dialog.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+                tvMessage = dialog.findViewById(R.id.tvMessage);
+                btnYes = dialog.findViewById(R.id.btn_Yes);
+                btnNo = dialog.findViewById(R.id.btn_No);
+
+                tvMessage.setText(message);
+
+                btnYes.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(getActivity(), LoginActivity.class);
+                        startActivity(intent);
+                    }
+                });
+
+                btnNo.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+
+                dialog.show();
+            }
+
+        });
         return view;
     }
 
